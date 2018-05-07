6f2b1cf4d2e9a33dabb49035f9d919a9893e49ef
fraoucene <fraoucene.guezout@gmail.com>
2018-05-07 17:42:07 +0200

    first commit

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,32 @@
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import TableList from "views/TableList/TableList.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+
+import {ContentPaste, Dashboard, Notifications} from "@material-ui/icons";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    sidebarName: "Dashboard",
+    navbarName: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage
+  },
+  {
+    path: "/table",
+    sidebarName: "Fomulaires de saisie",
+    navbarName: "Table List",
+    icon: ContentPaste,
+    component: TableList
+  },
+  {
+    path: "/notifications",
+    sidebarName: "Notifications",
+    navbarName: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage
+  },
+  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
+];
+
+export default dashboardRoutes;
